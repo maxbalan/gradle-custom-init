1ed37596a5d2c5bfd79fb3bd2297be38bfc0d522
Maxim Balan <maxim.balan@vonage.com>
2018-03-14 13:18:41 +0000

    fixing script to look for latest version of plugin

--- scripts/ExecuteCustomInit.sh
+++ scripts/ExecuteCustomInit.sh
@@ -117,7 +117,7 @@ if [ ! -f $GRADLE_PLUGIN/build.gradle ]; then
     echo `install -dv $GRADLE_PLUGIN`
 
     echo "plugins {
-            id 'com.github.maxbalan.gradle-custom-init' version '1.3.0'
+            id 'com.github.maxbalan.gradle-custom-init' version '1.3.1'
     }" >> "$GRADLE_PLUGIN/build.gradle"
 
     echo "" >> "$GRADLE_PLUGIN/settings.build"
